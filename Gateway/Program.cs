f83537833847934019d3970b4f91064b6c8b9c07
adeshrtk <adeshnyc2018@hotmail.com>
2026-03-13 21:44:55 +0530

    Ocelot API Gateway configured for both Identity and Weather Services

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,23 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+builder.Services.AddCors(options => { options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-//app.MapControllers();
+app.UseOcelot().Wait();
 
 app.Run();
